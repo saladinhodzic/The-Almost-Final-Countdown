f25face2d95aea4d6e9f542f80b4741957b9437a
saladinhodzic <hsaladin06@gmail.com>
2024-08-26 11:09:03 +0200

    Finishing the UserInput component

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,61 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  const [values, setValues] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 500,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setValues((prev) => {
+      return {
+        ...prev,
+        [inputIdentifier]: newValue,
+      };
+    });
+  };
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={values.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={values.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={values.expectedReturn}
+            onChange={(e) => handleChange("expectedReturn", e.target.value)}
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={values.duration}
+            onChange={(e) => handleChange("duration", e.target.value)}
+          />
         </p>
       </div>
     </section>
