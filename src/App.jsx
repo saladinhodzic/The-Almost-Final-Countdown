f4f0a274edbd0e84ea5d74f01d4e9841f642c655
saladinhodzic <hsaladin06@gmail.com>
2024-09-16 12:52:53 +0200

    Adding custom name using two-way binding

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <Player />
-      <div id="challenges">da</div>
+      <div id="challenges"></div>
     </>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submit, setSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    setSubmit(false);
+    setPlayerName(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setSubmit(true);
+  };
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submit ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
