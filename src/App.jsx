a11cd4e641f33e06be4a6b0e0ee85e1d434a9fb8
saladinhodzic <hsaladin06@gmail.com>
2024-09-16 12:31:04 +0200

    starting project

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import UserInput from "./components/UserInput";
-import Results from "./components/Results";
+import Player from './components/Player.jsx';
 
 function App() {
-  const [values, setValues] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 500,
-    expectedReturn: 6,
-    duration: 10,
-  });
-  const handleChange = (inputIdentifier, newValue) => {
-    setValues((prev) => {
-      return {
-        ...prev,
-        [inputIdentifier]: +newValue,
-      };
-    });
-  };
   return (
     <>
-      <Header />
-      <UserInput handleChange={handleChange} values={values} />
-      <Results values={values} />
+      <Player />
+      <div id="challenges"></div>
     </>
   );
 }
